2de8d4228f9d2007781de301165ca1c1878628b9
camilavib00 <camila.victoria@fu-berlin.de>
2024-09-04 14:39:07 +0200

    testing and debugging bubblesort

--- uebung_3_baselly_prahst.py
+++ uebung_3_baselly_prahst.py
@@ -11,14 +11,26 @@ Input: Unsortierte Liste von integers
 Output: Von klein nach groß sortierte Liste von integers
 Signatur: bubblesort: Liste[int] -> Liste[int]
 """
+def swap(val1, val2):
+    xs = [val2, val1]
+    return xs
 
 def bubblesort(xs):
-    xs_len = len(xs)-1                                          #value amount in list
-    for j in range(0, xs_len, 1):                               #4
-        for i in range(0, xs_len-1, 1):                         #1
-            if xs[i] > xs[i+1]:                                 #2
-                xs = xs[:i-1] + xs[i+1] + xs[i] + xs[i+2:]      #3
+    xs_len = len(xs)-1
+    print("Our List has ", xs_len+1, " elements in it! \n")                                     #value amount in list
+    for j in range(0, xs_len, 1):
+        print("We're at element: ", j, "\n")                               #4
+        for i in range(0, xs_len-1, 1):
+            print("comparing: ", xs[i], " with: ", xs[i+1], "\n")                         #1
+            if xs[i] > xs[i+1]:
+                print("xs[i] > xs[i+1]")                              #2
+                xs = xs[:i-1] + swap(xs[i+1],xs[i]) + xs[i+2:]  #3
+                print("current sorted list: ", xs)
+    return xs
 
+ls = [1, 8,5,9]
+print(ls)
+print(bubblesort(ls))
 """
 Aufgabe 3: Rekursion und Schleifen
 a)lovedDigits()
