dbbb58217a4c5a78efe861a48dfd76a53fe8d3de
camilavib00 <camila.victoria@fu-berlin.de>
2024-09-06 14:31:36 +0200

    add print statements for 1a) testing

--- uebung_4_baselly_prahst_python.py
+++ uebung_4_baselly_prahst_python.py
@@ -10,4 +10,10 @@ def foo(x):
     while x>0:
         y = str(x%2)+y
         x = x//2
+        print("updated y: ", y)
+        print("updated x: ", x)
     return y
+
+print("Lets find out what this function does :)")
+x  =int(input("Please enter your x: "))
+print(foo(x))
