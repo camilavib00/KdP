ec195b13faa66d120b08b5c64c19173218b4d2bd
camilavib00 <camila.victoria@fu-berlin.de>
2024-09-06 14:45:59 +0200

    1d) komplett und korrekt, print statements entfernt

--- uebung_4_baselly_prahst_python.py
+++ uebung_4_baselly_prahst_python.py
@@ -2,7 +2,6 @@
 Aufgabe 1:
 Laufzeitanalyse von foo
 geg: foo
-e) umschreiben in Rekursion
 """
 
 def foo(x):
@@ -10,10 +9,23 @@ def foo(x):
     while x>0:
         y = str(x%2)+y
         x = x//2
-        print("updated y: ", y)
-        print("updated x: ", x)
     return y
 
 print("Lets find out what this function does :)")
 x  =int(input("Please enter your x: "))
 print(foo(x))
+print("This function calculates the binary number of x!")
+
+#e) umschreiben in Rekursion
+
+def dezToBin(num):
+    #rekursionsanker
+    if num == 0:
+        return ''
+    #rekursionsschritt
+    y = str(num%2)
+    num = num//2
+    return dezToBin(num) + y
+
+num  =int(input("Please enter your num for the recursive version: "))
+print(dezToBin(num))
